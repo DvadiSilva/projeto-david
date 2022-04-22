45c6a0968ab5e86e8181b9d066c8f0a06ad4fcaf
Dvadi <david.s.vieira15@gmail.com>
2022-04-23 00:05:13 +0100

    Implementação do CSS da Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,38 @@
 import { NavLink } from "react-router-dom";
 
+import '../css/Navbar.css'
+
 export default function Navbar(){
     return(
         <header>
             <nav>
                 <h1>
                     <NavLink to="/">
-                        <img src="/images/logo/Dvadi_logo-preto" alt="logo"/>
+                        <img src="/images/logo/Dvadi_logo-preto.jpeg" alt="logo" className="logo"/>
                     </NavLink>
                 </h1>
                 <ul>
-                    <li>
+                    <li className="nav__list__item">
                         <NavLink to="/store">Loja Online</NavLink>
                     </li>
-                    <li>
+                    <li className="nav__list__item">
                         <NavLink to="/marcacoes">Marcações</NavLink>
                     </li>
-                    <li>
+                    <li className="nav__list__item">
                         <NavLink to="/precos">Preços</NavLink>
                     </li>
-                    <li>
+                    <li className="nav__list__item">
                         <NavLink to="/">Sobre Nós</NavLink>
                     </li>
-                    <li>
+                    <li className="nav__list__item">
                         <NavLink to="/">Contactos</NavLink>
                     </li>
-                    <li>
+                    <li className="nav__list__item">
                         <NavLink to="/">Cabelopédia</NavLink>
                     </li>
                     <li>
-                        <button type="button">
-                            <img src="/images/toa/hamburguer-menu.png"/>
+                        <button type="button" className="hamburguerBtn">
+                            <img src="/images/toa/hamburguer-menu.png" alt="nav expandida"/>
                         </button>
                     </li>
                 </ul>
