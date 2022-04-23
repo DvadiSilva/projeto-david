cf31a04001c7b6b96099146064899c9a6a07d578
Dvadi <david.s.vieira15@gmail.com>
2022-04-23 22:21:52 +0100

    Criação do componente SobreNos e o seu CSS responsive

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,13 @@
 import Hero from "./Hero";
 import Servicos from "./Servicos";
+import SobreNos from "./SobreNos";
 
 export default function Homepage(){
     return(
         <main>
             <Hero/>
             <Servicos/>
+            <SobreNos/>
         </main>
     );
 }
---
+++ src/components/SobreNos.jsx
@@ -0,0 +1,17 @@
+import '../css/SobreNos.css'
+
+export default function SobreNos(){
+    return(
+        <div className="sobre__nos">
+            <h2>Sobre Nós</h2>
+            <div className="sobre__nos__container">
+                <img src="images/toa/sobre-nos.png" className="sobre__nos__img" alt="icon sobre nós"/>
+                <div className="sobre__nos__text">
+                    <h3>Dvadi's</h3>
+                    <p>Fundado em 1998, por Dvadi Silva, empresário e dono das empresas DSV.</p>
+                    <p>Dvadi's foi o primeiro cabeleireiro na sua época a fazer o impossível, juntou num espaço, cabelos vindos de todas as partes do mundo, incluindo cabelos em guerra. Vindos de estados inimigos, encontravam aqui um local de paz e harmonia, o convívio era apreciado por todos, tornando-se o local de peregrinação escolhido.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
