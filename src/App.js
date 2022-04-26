6e3c2ab37a7191269daa63d247953f3303c8b66d
Dvadi <david.s.vieira15@gmail.com>
2022-04-26 16:05:57 +0100

    Reorganização do componente Media, nome e estrutura

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Marcacoes from './components/Marcacoes';
 import Precos from './components/Precos';
 
 import Navbar from './components/Navbar';
-import Media from './components/Media';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
           <Route path="/marcacoes" element={<Marcacoes/>}></Route>
           <Route path="/precos" element={<Precos/>}></Route>
         </Routes>
-        <Media/>
+        <Footer/>
       </div>
     </BrowserRouter>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import '../css/Footer.css'
+
+export default function Footer(){
+    return(
+        <footer>      
+            <div className="media">
+                <a href="#1">
+                    <img src="images/media/facebook.png" alt="facebook"/>
+                </a>
+                <a href="#1">
+                    <img src="images/media/instagram.png" alt="instagram"/>
+                </a>
+                <a href="#1">
+                    <img src="images/media/twitter.png" alt="twitter"/>
+                </a>
+            </div>
+        </footer>
+    );
+}
