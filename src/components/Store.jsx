c7d133f8843dc7df02a0239158876b6cfecd1677
Dvadi <david.s.vieira15@gmail.com>
2022-05-01 23:38:41 +0100

    Implementação da estrutura do componente Store e componente StoreItem e CSS essencial

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,5 +1,23 @@
+import {items} from "../produtos.js";
+import StoreItem from "./StoreItem.jsx";
+import '../css/Store.css';
+
+
 export default function Store(){
     return(
-        <div>Store</div>
+        <main className="store">
+            {
+                items.map(item=>(
+                    <StoreItem
+                        key={item.id}
+                        nome={item.nome}
+                        preco={item.preco}
+                        img={item.img}
+                        quantidade={item.quantidade}
+                        noCarrinho={item.noCarrinho}
+                    />
+                ))
+            }
+        </main>
     );
 }
