82eee23ca8d913351b866dbe292a7284213fa9e0
Dvadi <david.s.vieira15@gmail.com>
2022-05-11 22:25:47 +0100

    Atualização dos produtos da store e correções de CSS

--- src/produtos.js
+++ src/produtos.js
@@ -1,7 +1,16 @@
 export const items=[
     {
+        id:0,
+        nome:"Shampoo normal",
+        preco:8.5,
+        img:"images/toa/picture-icon.png",
+        quantidade:97,
+        noCarrinho:0,
+        subtotal:0
+    },
+    {
         id:1,
-        nome:"item1",
+        nome:"Shampoo p/ queda",
         preco:10,
         img:"images/toa/picture-icon.png",
         quantidade:67,
@@ -10,7 +19,7 @@ export const items=[
     },
     {
         id:2,
-        nome:"item2",
+        nome:"Shampoo p/ caspa",
         preco:20,
         img:"images/toa/picture-icon.png",
         quantidade:34,
@@ -19,7 +28,7 @@ export const items=[
     },
     {
         id:3,
-        nome:"item3",
+        nome:"Máscara Hidrantante Abacate",
         preco:13.5,
         img:"images/toa/picture-icon.png",
         quantidade:46,
@@ -28,7 +37,7 @@ export const items=[
     },
     {
         id:4,
-        nome:"item4",
+        nome:"Máscara Hidrantante Banana",
         preco:15,
         img:"images/toa/picture-icon.png",
         quantidade:88,
@@ -37,10 +46,109 @@ export const items=[
     },
     {
         id:5,
-        nome:"item5",
+        nome:"Máscara Hidrantante Cenoura",
         preco:7.5,
         img:"images/toa/picture-icon.png",
         quantidade:12,
+        noCarrinho:0,
+        subtotal:0
+    },
+    {
+        id:6,
+        nome:"Creme de Pentear Novex",
+        preco:9.5,
+        img:"images/toa/picture-icon.png",
+        quantidade:16,
+        noCarrinho:0,
+        subtotal:0
+    },
+    {
+        id:7,
+        nome:"Creme de Pentear Skala",
+        preco:7.5,
+        img:"images/toa/picture-icon.png",
+        quantidade:12,
+        noCarrinho:0,
+        subtotal:0
+    },
+    {
+        id:8,
+        nome:"Creme de Pentear Origem",
+        preco:7.5,
+        img:"images/toa/picture-icon.png",
+        quantidade:19,
+        noCarrinho:0,
+        subtotal:0
+    },
+    {
+        id:9,
+        nome:"Óleo de Argan Novex",
+        preco:5.5,
+        img:"images/toa/picture-icon.png",
+        quantidade:25,
+        noCarrinho:0,
+        subtotal:0
+    },
+    {
+        id:10,
+        nome:"Óleo de Ricino Novex",
+        preco:5.5,
+        img:"images/toa/picture-icon.png",
+        quantidade:43,
+        noCarrinho:0,
+        subtotal:0
+    },
+    {
+        id:11,
+        nome:"Óleo de Argan Skala",
+        preco:3.5,
+        img:"images/toa/picture-icon.png",
+        quantidade:23,
+        noCarrinho:0,
+        subtotal:0
+    },
+    {
+        id:12,
+        nome:"Óleo de Ricino Skala",
+        preco:3.5,
+        img:"images/toa/picture-icon.png",
+        quantidade:27,
+        noCarrinho:0,
+        subtotal:0
+    },
+    {
+        id:13,
+        nome:"Touca de cabelo Cetim",
+        preco:5.0,
+        img:"images/toa/picture-icon.png",
+        quantidade:32,
+        noCarrinho:0,
+        subtotal:0
+    },
+    {
+        id:14,
+        nome:"Fronha de Cetim",
+        preco:4.0,
+        img:"images/toa/picture-icon.png",
+        quantidade:54,
+        noCarrinho:0,
+        subtotal:0
+    },
+    {
+        id:15,
+        nome:"Secador de Cabelo",
+        preco:40,
+        img:"images/toa/picture-icon.png",
+        quantidade:5,
+        noCarrinho:0,
+        subtotal:0
+    },
+    {
+        id:16,
+        nome:"Secador de Cabelo c/difusor",
+        preco:47,
+        img:"images/toa/picture-icon.png",
+        quantidade:7,
         noCarrinho:0,
         subtotal:0
     },
